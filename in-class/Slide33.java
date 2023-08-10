cc61ecad1ead89a6b2073751bcfa5c2699c64cfa
ShanZeff <18shanzeff4@gmail.com>
2023-08-10 09:34:40 +0800

    commit

--- in-class/Slide33.java
+++ in-class/Slide33.java
@@ -6,7 +6,16 @@ public class Slide33 {
     int x2 = 4;
     double z = 0;
     
+    /*
     z = ((4*x + y) / x2) - 2*y;
+    z = (x + (y*y)) / 0.5;
+    z = x + (y*y);
+    z = Math.sqrt(x + (y*y));
+    double ans1 = Math.pow(y, 2);
+    z = Math.sqrt(x + ans1);
+    */
+    
+    z = Math.sqrt(x + Math.pow(y, 2));
     System.out.println(z);
   }
 }
---
+++ in-class/Slide40.java
@@ -0,0 +1,34 @@
+public class Slide40 {
+  public static void main (String[] args) {
+    // Slide 40 | 41 | 42
+    String bob = "bob";
+    /*
+    System.out.println(bob - 1);
+    System.out.println("hello"/123);
+    System.out.println(bob * "hello");
+    */
+    System.out.println("Hello, " + "world.");
+    System.out.println(bob + "ism");
+    
+    
+    // Slide 41
+    int hour = 11;
+    int minute = 59;
+    System.out.println(17*3);
+    System.out.println(hour*60 + minute);
+    int percentage;
+    percentage = (minute*100 / 60);
+    System.out.println(percentage);
+    
+    // Slide 42
+    //int x1 = 1.0;
+    int x2 = (int)1.1;
+    System.out.println(x2);
+    
+    // Slide 43: type-casting
+    double pi = 3.14159;
+    int x = (int)pi;
+    pi = x;
+    System.out.println(pi);
+  }
+}
