53fc083910f4cfd5e5c58e37314b6115c847b7eb
ShanZeff <18shanzeff4@gmail.com>
2023-08-17 01:25:15 +0800

    commit

---
+++ in-class/Draft01.java
@@ -0,0 +1,12 @@
+public class Draft01 {
+  public static void main(String[] args) {
+    // Lab Tasks/Exercises
+    // Task 6
+    float x1 = 1, x2 = 2, x3 = 4; 
+    float x4 = (x1 + x2 + x3) / 3;
+    System.out.println(x4);
+    
+    // Task 7
+    System.out.println("1 2 3 4");
+  }
+}
