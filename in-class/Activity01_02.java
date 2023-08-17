bd8cc262b3ed0e247ff0dcf645ae383e31b24186
ShanZeff <18shanzeff4@gmail.com>
2023-08-17 09:28:16 +0800

    last commit

--- in-class/Activity01_02.java
+++ in-class/Activity01_02.java
@@ -3,3 +3,12 @@ public class Activity01_02 {
     
   }
 }
+
+// shaded = big - small
+// big = 10 * 10
+// small = hyp * hyp
+// hyp = sqrt(5^2 + 5^2)
+
+// area triangle = 0.5 * b* h
+// area triangle = 0.5 * 5 * 5
+// area shaded = area triangle * 4
