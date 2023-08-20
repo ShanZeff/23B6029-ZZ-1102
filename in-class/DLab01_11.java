bac437c13e8a962afe14372b99c2c359f5bca6bb
ShanZeff <18shanzeff4@gmail.com>
2023-08-20 20:48:14 +0800

    phew

--- in-class/DLab01_11.java
+++ in-class/DLab01_11.java
@@ -1,7 +1,7 @@
 public class DLab01_11 {
   public static void main(String[] args) {
-    int x = 1+2+3+4+5+6+7+8+9+10;
-    System.out.println(x);
+    int x = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+    System.out.println("The sum is: " + x);
   }
 }
 
