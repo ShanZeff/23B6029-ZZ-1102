db00e713f2687ab4ce5415b91d0bb77f25a3434a
ShanZeff <18shanzeff4@gmail.com>
2023-08-29 07:57:47 +0800

    fix

--- in-class/DLab01_11.java
+++ in-class/DLab01_11.java
@@ -1,3 +1,4 @@
+/*
 public class DLab01_11 {
   public static void main(String[] args) {
     int x = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
@@ -6,3 +7,23 @@ public class DLab01_11 {
 }
 
 // sum of first ten positive integers
+*/
+import java.util.Scanner;
+
+public class DLab01_11 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter up to what number that you want to sum up: ");
+    int n = scanner.nextInt();
+    int result = sumUp(n);
+    System.out.println("Sum: " + result);
+  }
+  
+  public static int sumUp(int n) {
+    if (n==1) {
+      return 1;
+    } else {
+      return sumUp(n-1)+n;
+    }
+  }
+}
