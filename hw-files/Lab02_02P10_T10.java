40a1a8587387443afc43ec784c7b383640802a9d
ShanZeff <18shanzeff4@gmail.com>
2023-08-31 15:27:35 +0800

    yeah

--- hw-files/Lab02_02P10_T10.java
+++ hw-files/Lab02_02P10_T10.java
@@ -1,5 +1,17 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 public class Lab02_02P10_T10 {
+  public static final Scanner scanner = new Scanner(System.in);
+  
   public static void main(String[] args) {
+    double celsius, fahrenheit;
+    System.out.print("Enter the temperature in fahrenheit: ");
+    fahrenheit = scanner.nextDouble();
+    celsius = calcCelsius(fahrenheit); 
+    System.out.println("The temperature " + fahrenheit + " F is converted into " + celsius + " Celsius.");
+  }
+  
+  public static double calcCelsius(double myFahrenheit) {
+    double result = (5.0 / 9.0) * (myFahrenheit - 32);
+    return result;
   }
 }
--- hw-files/Lab02_02P10_T11.java
+++ hw-files/Lab02_02P10_T11.java
@@ -1,5 +1,27 @@
 //import java.util.Scanner;
 public class Lab02_02P10_T11 {
   public static void main(String[] args) {
+    double distance, speed, timeHour, timeMinute, timeTotal;
+    speed = 83.33;
+    timeHour = 1.0;
+    timeMinute = 12.0;
+    timeTotal = calcTime(timeHour, timeMinute);
+
+    distance = calcDistance(speed, timeTotal);
+    System.out.println(formatDecimal(distance));
+  }
+  
+  public static double calcTime(double myHour, double myMinute) {
+    double result = myHour + (myMinute / 60.0);
+    return result;
+  }
+  
+  public static double calcDistance(double mySpeed, double myTime) {
+    double result = mySpeed * myTime;
+    return result;
+  }
+  
+  public static String formatDecimal(double value) {
+    return String.format("%.2f", value);
   }
 }
