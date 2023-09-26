5f6f8a93b5ae09d10b62b18a132dc9214ae070b5
ShanZeff <18shanzeff4@gmail.com>
2023-09-26 08:07:13 +0800

    new class

---
+++ after-break/Slide10_00.java
@@ -0,0 +1,9 @@
+// Agenda: Week 7 - Strings
+// Exc: 10 LabString
+// String: not primitive - consists of char
+// Char: primitive data type
+
+public class Slide10_00 {
+  public static void main(String[] args) {
+  }
+}
