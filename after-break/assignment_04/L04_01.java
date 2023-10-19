14372cd07ad2360506500064b79e7cbae197c901
ShanZeff <18shanzeff4@gmail.com>
2023-10-19 21:04:57 +0800

    commit

--- after-break/assignment_04/L04_01.java
+++ after-break/assignment_04/L04_01.java
@@ -1,21 +1,38 @@
 // challenge 01
-// prompts and reads string
-// string: 3+ words
-// output string
-// string without: first and last
-
 import java.util.Scanner;
 public class L04_01 {
   public static final Scanner sc = new Scanner(System.in);
   public static void main(String[] args) {
-    String str = "red orange yellow green blue";
+    String str = getString();     // calls getString method and stores user input
+    printString(str);     // calls printString method and passes the user input into the method
+    sc.close();     // closes scanner
+  }
+  
+  public static String getString() {
+    // uses scanner to ask for the user to enter the words
+    // and returns the user input to the main method
+    System.out.print("Please enter three or more words: ");
+    String userInput = sc.nextLine();
+    return userInput;
+  }
+  
+  public static void printString(String str) {
+    // we can determine the start and end of a word by checking for the spaces
+    // identify the index where the space ' ' character appears the first time and the last time
     int firstSpace = str.indexOf(' ');
     int lastSpace = str.lastIndexOf(' ');
     
+    // if statement is used to make sure that the string has 3 or more words
     if (firstSpace != lastSpace) {
+      // this new string starts after the first occurence of the space character
+      // and this new string ends before the last occurence of the space character
       String newStr = str.substring(firstSpace+1, lastSpace);
+      // the system proceeds to print out the new string which excludes the first and last words
       System.out.println(newStr);
     } else {
+      // if firstSpace == lastSpace: this means that the user has only input one or two words
+      // here the system does not print out the new string 
+      // and displays a "Try Again." message to the user
       System.out.println("Try Again.");
     }
   }
--- after-break/assignment_04/L04_04.java
+++ after-break/assignment_04/L04_04.java
@@ -1,3 +1,34 @@
 // challenge 04
 public class L04_04 {
 }
+
+/*
+public class LA_04_03 {
+  // challenge 04
+  public static void main(String[] args) {
+    String fruit = "cool";
+    int index = 0;
+    int count = 0;
+    
+    while (count < 10) {
+      if (index < fruit.length() || index == 0) {
+        index = 0;
+        while (index < fruit.length()) {
+          char letter = fruit.charAt(index);
+          System.out.print(letter);
+          //index = index + 1;
+          //index = (index + 1) % fruit.length();
+          count = count + 1;
+        }
+      }
+    } 
+    
+    while (count < 10) {
+      char letter = fruit.charAt(index);
+      System.out.print(letter);
+      index = (index + 1) % fruit.length();
+      count = count + 1;
+    }
+  }
+}
+*/
