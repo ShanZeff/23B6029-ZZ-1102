c65bf87fcb8e4481e99a6b982041f05168ab20b8
ShanZeff <18shanzeff4@gmail.com>
2023-11-02 08:49:09 +0800

    commit

--- after-break/SlideOOP_01.java
+++ after-break/SlideOOP_01.java
@@ -1,9 +1,55 @@
+// TimeRunner program
 import java.util.Scanner;
 public class SlideOOP_01 {
   public static void main(String[] args) {
-    Time t = new Time(2, 4, 2.2);
-    System.out.println(t);
-    System.out.println(t.hour + ":" + t.minute + ":" + t.second);
-    t.print();
+    Time t1 = new Time();
+    t1.setHour(11);
+    t1.setMinute(8);
+    t1.setSecond(3.14159);
+    t1.print();
+    
+    Time t2 = new Time();
+    t2.setAll(9, 17, 1.0);
+    t2.print();
+    
+    Time t3 = new Time(2, 54, 2.2);
+    printTime(t3);
+    
+    Time t4 = new Time(8, 7, 4.5);
+    System.out.println(t4.hour + ":" + t4.minute + ":" + t4.second);
+    
+    boolean result = isAfter(t4, t3);
+    System.out.println(result);
+    
+    Time sum = addTime(t2, t3);
+    sum.print();
+  }
+  
+  public static void printTime(Time t) {
+    System.out.println(t.getHour() + ":" + t.getMinute() + ":" + t.getSecond());
+  }
+  
+  public static boolean isAfter(Time time1, Time time2) {
+    if (time1.hour > time2.hour) return true;
+    if (time1.hour < time2.hour) return false;
+    if (time1.minute > time2.minute) return true;
+    if (time1.minute < time2.minute) return true;
+    if (time1.second > time2.second) return true;
+    if (time1.second < time2.second) return true;
+    return false;
+  }
+  
+  public static Time addTime(Time t1, Time t2) {
+    Time sum = new Time();
+    sum.hour = t1.hour + t2.hour;
+    sum.minute = t1.minute + t2.minute;
+    sum.second = t1.second + t2.second;
+    if (sum.second >= 60.0) {
+      sum.second -= 60.0;
+      sum.minute += 1;
+    } if (sum.minute >= 60) {sum.minute -= 60;
+      sum.hour += 1;
+    }
+    return sum;
   }
 }
--- after-break/Time.java
+++ after-break/Time.java
@@ -5,6 +5,13 @@ public class Time {
   
   // constructor - is use to initialise the instance variable
   // must not have void
+  // instance variable with default values
+  public Time() {
+    this.hour = 0;
+    this.minute = 0;
+    this.second = 0.0;
+  }
+  
   public Time(int h, int m, double s) {
     hour = h;
     minute = m;
@@ -15,5 +22,35 @@ public class Time {
   // get() and set()
   public void print() {
     System.out.println(hour + ":" + minute + ":" + second);
+  }
+  
+  // set() methods
+  public void setHour(int hr) {
+    this.hour = hr;
+  }
+  
+  public void setMinute(int min) {
+    this.minute = min;
+  }
+  
+  public void setSecond(double sec) {
+    this.second = sec;
+  }
+  
+  public void setAll(int hr, int min, double sec) {
+    this.hour = hr; this.minute = min; this.second = sec;
+  }
+  
+  // get() methods
+  public int getHour() {
+    return this.hour;
+  }
+  
+  public int getMinute() {
+    return this.minute;
+  }
+  
+  public double getSecond() {
+    return this.second;
   }
 }
